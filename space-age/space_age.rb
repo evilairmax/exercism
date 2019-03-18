554c6a041435424a77e9ec55e37293f8a7e6df6b
Rodolfo Thiede <hello@rodolfothiede.me>
2019-03-18 01:22:44 -0300

    Solved 'space age' challenge'

---
+++ space-age/space_age.rb
@@ -0,0 +1,17 @@
+class SpaceAge
+  planets = {
+      :earth => 1.0,
+      :mercury => 0.2408467,
+      :venus => 0.61519726,
+      :mars => 1.8808158,
+      :jupiter => 11.862615,
+      :saturn => 29.447498,
+      :uranus => 84.016846,
+      :neptune => 164.79132
+  }
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  planets.each {|name, value| define_method("on_#{name}") { @seconds / (31_557_600 * value) } }
+end
